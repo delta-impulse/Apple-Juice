310961c791df7092727624f42b7c0f9b3b61e262
Christian Andriss <andriss.christian@gmail.com>
2020-04-18 19:30:37 +0200

    Initial Commit

---
+++ AppleJuice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleJuice
+//
+//  Created by Christian Andriß on 18.04.20.
+//  Copyright © 2020 Christian Andriß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
