907ee7fdbf4246f0a88f4834fe75d5141df9967c
Christian Andriss <andriss.christian@gmail.com>
2020-04-18 23:04:20 +0200

    Added first functionality

--- AppleJuice/ViewController.swift
+++ AppleJuice/ViewController.swift
@@ -15,6 +15,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var calcNumber: UILabel!
+    
+    @IBAction func numberClicked(_ sender: UIButton) {
+        //weak var calcNumber: UILabel!
+        
+        var number:String = sender.currentTitle!
+        
+        var currentNumber:String = calcNumber.text!
+        
+        var floatNumber:Float = Float(number)!
+        
+        var floatCurrentNumber: Float = Float(currentNumber)!
+        
+        calcNumber.text = String(floatCurrentNumber+floatNumber)
+        
+    }
+    
+    
+    
+    
 }
 
